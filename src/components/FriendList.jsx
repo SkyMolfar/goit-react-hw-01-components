2ed3d314bf94d42d037e3ae707228dccb95b592f
SkyMolfar <skymolfars@gmail.com>
2023-07-05 08:51:59 +0200

    add friendsListItem

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,22 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styled/FriendList.css';
+import FriendListItem from './FriendListItem';
 
-const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (
-    <li className="item">
-      <span className={`status ${isOnline ? 'online' : 'offline'}`}></span>
-      <img className="avatar" src={avatar} alt="User avatar" width="48" />
-      <p className="name">{name}</p>
-    </li>
-  );
-};
-
-FriendListItem.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
-};
 
 const FriendList = ({ friends }) => {
   return (
